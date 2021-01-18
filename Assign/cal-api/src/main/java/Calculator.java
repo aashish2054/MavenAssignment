79d26715392687f82538efec7bcbfcbfe83c4b1c
aashish2054 <ashish.chaudhary@knoldus.com>
2021-01-19 00:00:04 +0530

    Maven Assignment by Ashish Chaudhary

---
+++ Assign/cal-api/src/main/java/Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+    public static void Multiplication(int integer1 , int integer2){
+        int integer3 =integer1*integer2;
+        System.out.println("Multiplication is " + integer3);
+    }
+    public static void Division(int integer1 , int integer2){
+        int integer3 =integer1/integer2;
+        System.out.println("Division is " + integer3);
+    }
+    public static void Addition(int integer1 , int integer2){
+        int integer3 =integer1+integer2;
+        System.out.println("Sum is " + integer3);
+    }
+    public static void Substraction(int integer1 , int integer2){
+        int integer3 =integer1-integer2;
+        System.out.println("Substraction is" + integer3);
+    }
+
+}
---
+++ Assign/cal-impl/src/main/java/Application.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String args[]){
+
+
+        Calculator calci = new Calculator();
+        calci.Multiplication(200,100);
+        calci.Division(200,100);
+        calci.Addition(200,100);
+        calci.Substraction(200,100);
+
+    }
+}
